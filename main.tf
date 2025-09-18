9c576f087917a99f6d3bebfbcd09cbc4bb362a84
shankarvenkat1998-sketch <shankarvenkat1998@gmail.com>
2025-09-18 12:07:37 +0530

    third commit

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = { 
+      source = "hashicorp/aws"
+      version = "6.12.0"
+    }
+  }
+}
 resource "aws_vpc" "shankarvpc" {
   cidr_block = "10.10.0.0/16"
   tags = {
